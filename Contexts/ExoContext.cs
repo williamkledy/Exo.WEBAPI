e4fbac81666c4b9033511ac0dd1e6416e55e3e90
williamkledy <wkledy@hotmail.com>
2023-11-29 23:01:01 -0300

    restante Crud e insomnia

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -19,12 +19,13 @@ protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         if (!optionsBuilder.IsConfigured)
     {
 // Essa string de conexão depende da SUA máquina.
-optionsBuilder.UseSqlServer ("server=localhost\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True");
+optionsBuilder.UseSqlServer("User ID=sa;Password=12345;Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=false;");
 // Exemplo 1 de string de conexão:
 //User ID= sa;Password=12345;Server=localhost;Database=ExoApi + Trusted_Connection =false;
 // Exemplo 2 de string de conexão:
 //
   //Server='localhost\NOTEACER\SQLEXPRESS\ ;Database=ExoApi;Trusted_Connection=True;'
+  //"server=localhost\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True");
 }
 }
 public DbSet<Projeto> Projetos { get ; set ;}
--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace Exo.WebApi.Controllers
 {
-    [Route("api/controller")]
+    [Route("api/[controller]")]
     [ApiController]
 
     public class ProjetosController : ControllerBase
@@ -21,6 +21,41 @@ public ProjetosController (ProjetoRepository projetoRepository)
  public IActionResult Listar()
 {
 return Ok (_projetoRepository.Listar());
+}
+[HttpPost]
+public IActionResult Cadastrar(Projeto projeto)
+{
+   _projetoRepository.Cadastrar(projeto);
+    return StatusCode(201);
+}
+[HttpGet("{id}")]
+public IActionResult BuscarPorId(int id)
+{
+Projeto projeto= _projetoRepository.BuscarporId(id);
+if(projeto== null)
+{
+return NotFound();
+}
+return Ok(projeto);
+}
+[HttpPut("{id}")]
+public IActionResult Atualizar(int id, Projeto projeto)
+{
+_projetoRepository.Atualizar(id, projeto);
+return StatusCode(204);
+}
+[HttpDelete("{id}")]
+public IActionResult Deletar(int id)
+{
+try
+{
+_projetoRepository.Deletar(id);
+return StatusCode(204);
+}
+catch(Exception e)
+{
+return BadRequest();
+}
 }
 }
 }
